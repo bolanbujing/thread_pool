81fb0ba3af1e6ec102ea98cee6fc8c153e1a13ac
xiaobo.liu <xiaobo.liu@lingshu.net>
2023-01-16 16:41:55 +0800

    无锁队列

---
+++ lock_free_queue.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <boost/lockfree/queue.hpp>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <functional>
+
+int main() {
+    using Task = int;
+    boost::lockfree::queue<Task> que(512);
+    std::atomic<int> at{0};
+    std::vector<std::thread> readers;
+    for(int i = 0; i < 4; i++) {
+        readers.push_back(std::thread([&que](){
+            for(;;) {
+                while(!que.empty()) {
+                    Task val;
+                    if(que.pop(val)) {
+                        std::cout << val << std::endl;
+                    }
+                }
+            }
+        }));
+    }
+    std::vector<std::thread> writers;
+    for(int i = 0; i < 4; i++) {
+        writers.push_back(std::thread([&que, &at](){
+            for(int x=0; x < 30; x++) {
+                que.push(at.fetch_add(1));
+            }
+        }));
+    }
+    for(auto& item : readers) {
+        item.join();
+    }
+    for(auto& item : writers) {
+        item.join();
+    }
+    return 0;
+}
